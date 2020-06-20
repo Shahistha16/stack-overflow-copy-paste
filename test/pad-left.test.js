004240215efc9aef26303ae4aa5637af9cef0a60
Shahistha S <shahistha.sheikh@robosoftin.com>
2020-06-20 18:59:12 +0530

    I need help with the commit message

--- test/pad-left.test.js
+++ test/pad-left.test.js
@@ -1,5 +1,5 @@
 import test from 'ava'
-import {padLeft} from '../src'
+import { padLeft } from '../src'
 
 test('pads left of the given string', t => {
   const original = '123'
@@ -23,5 +23,14 @@ test('does not pad a string longer than the pad length', t => {
   const expected = '1234'
   const padLength = 3
   const actual = padLeft(original, padLength)
+  t.deepEqual(actual, expected)
+})
+
+test('pads left of the given string---', t => {
+  const original = '123'
+  const expected = 'zz123'
+  const padLength = 5
+  const padWith = 'z'
+  const actual = padLeft(original, padLength, padWith)
   t.deepEqual(actual, expected)
 })
